ffecb7ac4f039d146bb599fe3b5d51e34517e527
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-04-13 17:48:08 -0500

    add persistant feature

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import pickle
+from os import path
+
+
 # def get_instruction_hash(instructions):
 #     to_hash = [str((ins.opcode, ins.argval)) for ins in instructions]
 #     hash_str = ' '.join(to_hash).encode('utf-8')
@@ -14,15 +18,27 @@
 #
 
 def smart_cache(input_func):
+    func_name = input_func.__name__
+    cache_file_name = func_name + '.pickle'
     cache = {}
+
+    if path.exists(cache_file_name):
+        with open(cache_file_name, 'rb') as cache_file:
+            cache = pickle.load(cache_file)
+
 
     def wrapper(*args, **kwargs):
         frozen_set = frozenset(kwargs.items())
         hash_input = hash((*args, *frozen_set))
         if hash_input in cache:
+            print("cached")
             return cache[hash_input]
         result = input_func(*args, **kwargs)
         cache[hash_input] = result
+
+        with open(cache_file_name, 'wb') as cache_file:
+            pickle.dump(cache, cache_file, protocol=4)
+
         return result
 
     return wrapper
@@ -35,4 +51,4 @@ def function_to_cache(a):
 
 if __name__ == "__main__":
     function_to_cache(1)
-    function_to_cache(1)
+    # function_to_cache(1)
