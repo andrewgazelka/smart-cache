9a914854c6905e6bb01b4534c40eab72362de895
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-04-14 16:20:58 -0500

    setup on pypi

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="smart_cache",
+    version="1.0.0",
+    description="Python caching library that is persistent and uses bytecode analysis to determine re-evaluation",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andrewgazelka/smart-cache",
+    author="Andrew Gazelka",
+    author_email="andrew.gazelka@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["smart_cache"],
+    include_package_data=True,
+    install_requires=[],
+)
